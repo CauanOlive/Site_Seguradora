709c4074f503ff699857535ec40951a42aa64170
KevinFBenicio <149635211+KevinFBenicio@users.noreply.github.com>
2023-11-01 21:31:39 -0300

    Update Contact.php

--- Contact.php
+++ Contact.php
@@ -7,24 +7,28 @@
     <title>Página de Contato</title>
 </head>
 <body>
-    <header>
-        <h1>Entre em Contato</h1>
-    </header>
-    <nav>
+    <div id=cabeca>
+
+        <h1>Entre em contato e escolha como quer falar com agente:</h1>
+        
         <ul>
-            <li><a href="index.php">Página Principal</a></li>
+            <li id="Principal"><a href="index.html">Página Principal</a></li>
         </ul>
-    </nav>
-    <section>
-        <h2>Informações de Contato</h2>
-        <p>Para entrar em contato conosco, utilize os seguintes meios:</p>
-        <ul>
-            <li>Email: <a href="mailto:ProtecSeguro@gmail.com.br">ProtecSeguro@gmail.com.br</a></li>
-            <li>Telefone: (11) 1234-5678</li>
-        </ul>
-    </section>
-    <footer>
-        <p>&copy; 2023 ProtecSeguro</p>
-    </footer>
-</body>
+    </div>
+
+    <div id="corpo">
+        <section>
+            <p><b>Para entrar em contato conosco utilize os seguintes meios:</b></p>
+            <ul>
+                <div id="email">
+                    <li>Email: <a href="mailto:ProtecSeguro@gmail.com.br">ProtecSeguro@gmail.com.br</a></li>
+                </div>
+                <div id="telefone">
+                    <li id="telefone">Telefone: (11) 1234-5678</li>
+                </div>
+            </ul>
+        </section>
+    </div>
+        <p2>&copy; 2023 ProtecSeguro</p>
+    </body>
 </html>
