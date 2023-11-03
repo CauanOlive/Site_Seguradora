10e423f691c9f9d69ea67a31bfb70576016b7916
KevinFBenicio <149635211+KevinFBenicio@users.noreply.github.com>
2023-11-03 08:19:15 -0300

    Update Group.php

--- Group.php
+++ Group.php
@@ -10,28 +10,31 @@
     <header>
         <h1>Membros do Grupo</h1>
     </header>
-    <nav>
-        <ul>
-            <li><a href="index.html">Página Principal</a></li>
-        </ul>
-    </nav>
+    <hr>
+    <div>
+        <a href="index.html">Página Principal</a></li>    
+    </div>
     <section>
-        <h2>Membros e Funções</h2>
+        <h2>Membros e Funções:</h2>
         <p>Aqui estão os membros do nosso grupo e suas funções:</p>
         <ul>
             <li><strong>Cauan</strong> - Engenheiro de Software</li>
             <li><strong>Lucas</strong> - Desenvolvedor</li>
             <li><strong>Pedro</strong> - Designer</li>
-            <!-- Adicione mais membros e funções conforme necessário -->
+            <li><strong>Kevin</strong> - Designer</li>
         </ul>
     </section>
     <section>
-        <h2>Estruturas e Controle de Sessões</h2>
-        <p></p>
+        <h2>Estruturas e Controle de Sessões:</h2>
+        <p>Contato</p>
+        <p>Seguro</p>
+        <p>Index</p>
+        <p>Membros do grupo</p>
     </section>
-    <footer>
+    <div id=Final>
         <p>&copy; 2023 ProtecSeguro</p>
-    </footer>
+    </div>
+    
 </body>
 </html>
 
