308265dc5a69cf9310606dcab8eb4850e02e868d
KevinFBenicio <149635211+KevinFBenicio@users.noreply.github.com>
2023-11-03 13:27:10 -0300

    Update index.php

--- index.php
+++ index.php
@@ -20,39 +20,43 @@
         </nav>
     </header>
 
-    <section class="hero">
+    <div class="hero">
         <h2>Proteja o seu veículo com o melhor seguro do mercado!</h2>
-        <p>Oferecemos cobertura abrangente para carros e preços competitivos.</p>
-      </section>
-
-    <section class="about">
-        <h2>Sobre Nós</h2>
-        <p>A ProtecSeguro é uma empresa líder em seguros automotivos, com anos de experiência e milhares de clientes satisfeitos. <br> Nossa missão é fornecer proteção confiável e acessível para o seu veículo.</p>
-    </section>
-
-    <section class="services">
-        <h2>Nossos Serviços</h2>
-        <ul>
-            <li>Seguro de Responsabilidade Civil</li>
-           <p>Proteção contra danos a terceiros e suas propriedades.</p>
-            <li>Seguro contra Colisões</li>
-          <p>Cobertura para danos ao seu veículo em acidentes.</p>
-            <li>Seguro contra Danos Compreensivos</li>
-           <p>Proteção total, incluindo colisões, furto e outros
-            <li>Assistência 24 Horas</li>
-             <p>Guincho 24 Horas, sem limite de quilometragem.</p>
-        </ul>
-      <a href="form.php" class="cta-button">Obter Cotação Grátis</a>
-    </section>
+        <p2>Oferecemos cobertura abrangente para carros e preços competitivos.</p2>
+    </div>
+      
+        <div class="quadrado">
+            <h3>Sobre Nós</h3>
+            <p3>A ProtecSeguro é uma empresa líder em seguros automotivos,<br></p3><p3>com anos de experiência e milhares de clientes satisfeitos.Nossa<br></p3> <p3> missão é fornecer proteção confiável e acessível para o seu veículo.</p3>
+        
+        </div>
+        <div class="quadrados">
+            <div class="comeco">
+                <h3>Nossos Serviços:</h3>
+            </div>
+                <ul>
+                    <li>Seguro de Responsabilidade Civil</li>
+                <p>Proteção contra danos a terceiros e suas propriedades.</p>
+                    <li>Seguro contra Colisões</li>
+                <p>Cobertura para danos ao seu veículo em acidentes.</p>
+                    <li>Seguro contra Danos Compreensivos</li>
+                <p>Proteção total, incluindo colisões, furto e outros
+                    <li>Assistência 24 Horas</li>
+                    <p>Guincho 24 Horas, sem limite de quilometragem.</p>
+                </ul>
+            <a href="form.php" class="cta-button1">Obter Cotação Grátis</a>
+        </div>
 
     <section class="contact">
-        <h2>Entre em Contato</h2>
-        <p>Tem alguma dúvida? Fale conosco para obter mais informações ou para receber uma cotação personalizada para o seu veículo.</p>
-        <a href="Contact.php" class="cta-button">Contato</a>
+        <h4>Entre em Contato</h4>
+        <p4>Tem alguma dúvida? Fale conosco para obter mais informações ou para receber uma cotação personalizada para o seu veículo.</p4>
+        <br>
+        </br>
+        <a href="Contact.php" class="cta-button2">Contato</a>
     </section>
 
     <footer>
-        <p>&copy; 2023 ProtecSeguro. Todos os direitos reservados.</p>
+        <p1>&copy; 2023 ProtecSeguro. Todos os direitos reservados.</p1>
     </footer>
 </body>
 </html>
