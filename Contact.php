45642d503145d9809084762f43845aceecf3685d
Cauan Oliveira <cauanog007@gmail.com>
2023-11-05 14:07:13 -0300

    Update Contact.php

--- Contact.php
+++ Contact.php
@@ -7,28 +7,25 @@
     <title>Página de Contato</title>
 </head>
 <body>
-    <div id=cabeca>
-
-        <h1>Entre em contato e escolha como quer falar com agente:</h1>
-        
+    <header>
+        <h1>Entre em Contato</h1>
+    </header>
+    <nav>
         <ul>
-            <li id="Principal"><a href="index.html">Página Principal</a></li>
+            <li><a href="index.php">Página Principal</a></li>
         </ul>
-    </div>
-
-    <div id="corpo">
-        <section>
-            <p><b>Para entrar em contato conosco utilize os seguintes meios:</b></p>
-            <ul>
-                <div id="email">
-                    <li>Email: <a href="mailto:ProtecSeguro@gmail.com.br">ProtecSeguro@gmail.com.br</a></li>
-                </div>
-                <div id="telefone">
-                    <li id="telefone">Telefone: (11) 1234-5678</li>
-                </div>
-            </ul>
-        </section>
-    </div>
-        <p2>&copy; 2023 ProtecSeguro</p>
-    </body>
+    </nav>
+    <section>
+        <h2>Informações de Contato</h2>
+        <p>Para entrar em contato conosco, utilize os seguintes meios:</p>
+        <ul>
+            <li>Email: <a href="mailto:ProtecSeguro@gmail.com.br">ProtecSeguro@gmail.com.br</a></li>
+            <li>Ou fale com um de nossos atendentes: <br>
+            Telefone: (11) 1234-5678</li>
+        </ul>
+    </section>
+    <footer>
+        <p>&copy; 2023 ProtecSeguro</p>
+    </footer>
+</body>
 </html>
