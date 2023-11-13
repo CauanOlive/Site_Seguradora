b807f78aca319c10fe4d60966e1f9b1f200706f1
Cauan Oliveira <cauanog007@gmail.com>
2023-11-13 18:48:08 -0300

    Add files via upload

--- Group.php
+++ Group.php
@@ -1,39 +1,39 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styleGroup.css">
-    <title>Membros do Grupo</title>
-</head>
-<body>
-    <header>
-        <h1>Membros do Grupo</h1>
-    </header>
-    <hr>
-    <section>
-        <h2>Membros e Funções:</h2>
-        <p>Aqui estão os membros do nosso grupo e suas funções:</p>
-        <ul>
-            <li><strong>Cauan  RGM:33033081</strong> - Engenheiro de Software</li>
-            <li><strong>Lucas RGM:32958838</strong> - Desenvolvedor</li>
-            <li><strong>Pedro RGM:33019568</strong> - Designer</li>
-            <li><strong>Kevin RGM:32705301</strong> -  Web Designer</li>
-        </ul>
-    </section>
-    <section>
-        <h2>Estruturas e Controle de Sessões:</h2>
-        
-        <a href="pg_principal.php">Página Principal</a> <br><br>
-        <a href="contact.php"> Contato</a> <br><br>
-        <a href="form.php"> Seguro e formúlario</a> <br><br>
-        <a href="Index.php"> Index </a> <br><br>
+    <!DOCTYPE html>
+    <html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="styleGroup.css">
+        <title>Membros do Grupo</title>
+    </head>
+    <body>
+        <header>
+            <h1>Membros do Grupo</h1>
+        </header>
+        <hr>
+        <section>
+            <h2>Membros e Funções:</h2>
+            <p>Aqui estão os membros do nosso grupo e suas funções:</p>
+            <ul>
+                <li><strong>Cauan  RGM:33033081</strong> - Engenheiro de Software</li>
+                <li><strong>Lucas RGM:32958838</strong> - Desenvolvedor</li>
+                <li><strong>Pedro RGM:33019568</strong> - Designer</li>
+                <li><strong>Kevin RGM:32705301</strong> -  Web Designer</li>
+            </ul>
+        </section>
+        <section>
+            <h2>Estruturas e Controle de Sessões:</h2>
+            
+            <a href="pg_principal.php">Página Principal</a> <br><br>
+            <a href="contact.php"> Contato</a> <br><br>
+            <a href="form.php"> Seguro e formúlario</a> <br><br>
+            <a href="Index.php"> Index </a> <br><br>
 
-    </section>
-    <footer>
-        <p>&copy; 2023 ProtecSeguro</p>
-    </footer>
-    
-</body>
-</html>
+        </section>
+        <footer>
+            <p>&copy; 2023 ProtecSeguro</p>
+        </footer>
+        
+    </body>
+    </html>
 
